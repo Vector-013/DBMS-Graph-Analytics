b876f7ac502df04d84849142eaff8f9e71c6407a
gauravroy04 <roygaurav2004@kgpian.iitkgp.ac.in>
2025-04-05 21:45:02 +0530

    second query

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,40 +1,3 @@
-// import { useNavigate } from "react-router-dom";
-// import "../styles/HomePage.css"; // Import the separate CSS
-
-// const LandingPage = () => {
-//   const navigate = useNavigate();
-
-//   const pages = [
-//     { name: "Shortest Path Finder", path: "/path-visualizer" },
-//     { name: "Page 2", path: "/page2" },
-//     { name: "Page 3", path: "/page3" },
-//     { name: "Page 4", path: "/page4" },
-//     { name: "Page 5", path: "/page5" },
-//     { name: "Page 6", path: "/page6" },
-//     { name: "Page 7", path: "/page7" },
-//     { name: "Page 8", path: "/page8" },
-//     { name: "Page 9", path: "/page9" },
-//   ];
-
-//   return (
-//     <div className="landing-container">
-//       <h1 className="landing-title">Welcome to the Landing Page</h1>
-//       <div className="row">
-//         {pages.map((page, index) => (
-//           <div className="col" key={index}>
-//             <div className="card">
-//               <h2>{page.name}</h2>
-//               <button onClick={() => navigate(page.path)}>Go to {page.name}</button>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LandingPage;
-
 import { useNavigate } from "react-router-dom";
 import "../styles/HomePage.css";
 
@@ -54,7 +17,7 @@ const LandingPage = () => {
           </div>
 
           <div className="card">
-            <h2>Page 2</h2>
+            <h2>One Hop Neighbours</h2>
             <button onClick={() => navigate("/connected-nodes")}>Visit</button>
           </div>
 
