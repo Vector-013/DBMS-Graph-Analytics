8539b456d06bdb7bf1489803d30fd567270b3a10
VISTRINNA <vistrinnag@gmail.com>
2025-04-05 14:52:12 +0530

    shortest path ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,9 @@ import PathVisualizer from './components/PathVisualizer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>LastFM User Network Shortest Path</h1>
-        <p>Find the shortest path between two users in the network</p>
-      </header>
-      <main>
-        <PathVisualizer />
-      </main>
-      <footer>
-        <p>Neo4j Graph Database Visualization with Cytoscape.js</p>
-      </footer>
+      
+      <PathVisualizer />
+      
     </div>
   );
 }
